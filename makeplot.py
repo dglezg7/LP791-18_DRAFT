96b4caeec2fc9194915c81fd5dec770d57e81462
David Graham <58270448+dglezg7@users.noreply.github.com>
2023-08-30 17:43:14 +0200

    Add files via upload
    updated makeplot.py

--- makeplot.py
+++ makeplot.py
@@ -1,3 +1,4 @@
+from audioop import add
 import pathlib
 import sys
 
@@ -12,106 +13,98 @@ import vplanet
 # Path hacks
 path = pathlib.Path(__file__).parents[0].absolute()
 sys.path.insert(1, str(path.parents[0]))
-#from get_args import get_args
-
-#planetName = sys.argv[2]
-
-class Planet:
-    a = 0.0
-    e = 0.0
-    i = 0.0
-    LongP = 0.0
-    LongA = 0.0
-    MeanA = 0.0
-    x1 = 0.0
-    x2 = 0.0
-    x3 = 0.0
-    v1 = 0.0
-    v2 = 0.0
-    v3 = 0.0
-    Time = []
-    Mass = 0.0
-
-
-def ReadHNBody(FileName):
-    planet = Planet()
-    (
-        planet.Time,
-        planet.a,
-        planet.e,
-        planet.i,
-        planet.LongP,
-        planet.LongA,
-        planet.MeanA,
-        planet.x1,
-        planet.x2,
-        planet.x3,
-        planet.v1,
-        planet.v2,
-        planet.v3,
-    ) = np.loadtxt(FileName, skiprows=17, unpack=True)
-    planet.MeanA[planet.MeanA < 0] += 360
-    planet.LongP[planet.LongP < 0] += 360
-    planet.LongA[planet.LongA < 0] += 360
-    planet.a = planet.a
-    return planet
-
 
 # Run vplanet
-SS = vplanet.run(path / "vpl.in", units=False, quiet=True)
+# Runs vplanet and takes the bodies
+SS = vplanet.run(path / "vpl.in", units=False, quiet=True) 
+bodies = SS.bodies # takes the info of all the bodies
 
-# Get hnbody results
-#hnb = ReadHNBody(path / "b.dat")
-#hnb.Mass = 1.0
+# Array that takes the name of all the bodies
+acBodiesNames = [str(bodies[iBody]).split(" ")[-1][:-1] 
+                    for iBody in range(len(bodies))]
 
-fig, ([ax1, ax2], [ax3, ax4], [ax5, ax6]) = plt.subplots(3, 2, figsize=[14, 16])
+# Below we introduce figure axis unit labels
+cUnitMass = ""
+cUnitLength = ""
+cUnitTime = "" 
+cUnitAngle = ""
+cUnitTemp = ""
 
-#ax1.plot(hnb.Time, hnb.a, color=vpl.colors.red)
-ax1.plot(SS.b.Time, SS.b.SemiMajorAxis, "k")
-ax1.set_rasterization_zorder(0)
-ax1.set_ylabel("Semi-Major Axis (AU)")
-ax1.yaxis.set_major_formatter(FormatStrFormatter("%.6f"))
+# Adds unit from vpl.in file
+def addUnit(cUnit, cLabel, TmpLine):
+    #print("TmpLine:", TmpLine)
+    if len(TmpLine) > 0 and TmpLine[0] == cLabel and len(cUnit) == 0:
+        cUnit = TmpLine[1]
+    return cUnit
 
-#ax2.plot(hnb.Time, hnb.e, color=vpl.colors.red)
-ax2.plot(SS.b.Time, SS.b.Eccentricity, "k")
-ax2.set_ylabel("Eccentricity")
+# Open the vpl.in file in the same directory as the script
+with open('vpl.in', 'r') as file:
+    lines = file.readlines()
 
-plt.subplot(3, 2, 3)
-#plt.plot(hnb.Time, hnb.i, color=vpl.colors.red)
-plt.plot(SS.b.Time, SS.b.Inc, "k")
-plt.legend(["HNBody", "VPLanet"], loc="upper left")
-plt.ylabel("Inclination ($^\circ$)")
+# Iterate through each line in the file
+for line in lines:
+    # Split the line into words
+    words = line.split()
 
-plt.subplot(3, 2, 4)
-#plt.plot(hnb.Time, hnb.LongP, color=vpl.colors.red, marker=".", linewidth=0)
-plt.plot(SS.b.Time, SS.b.LongP, "k", marker=".", linewidth=0)
-plt.xlabel("Time (yrs)")
-plt.ylim(0, 360)
-plt.ylabel("Longitude of Pericenter ($^\circ$)")
+    # Check if the line contains the following keywords
+    cUnitMass = addUnit(cUnitMass, "sUnitMass", words)
+    cUnitLength = addUnit(cUnitLength, "sUnitLength", words)
+    cUnitTime = addUnit(cUnitTime, "sUnitTime", words)
+    cUnitAngle = addUnit(cUnitAngle, "sUnitAngle", words)
+    cUnitTemp = addUnit(cUnitTemp, "sUnitTemp", words)
 
-plt.subplot(3, 2, 5)
-#plt.plot(hnb.Time, hnb.LongA, color=vpl.colors.red, marker=".", linewidth=0)
-plt.plot(SS.b.Time, SS.b.LongA, color="k", marker=".", linewidth=0)
-plt.ylim(0, 360)
-plt.ylabel("Longitude of Ascending Node ($^\circ$)")
-plt.xlabel("Time (yrs)")
-
-
-#etot = (SS.star.TotOrbEnergy / SS.star.TotOrbEnergy[0] - 1) * 1e9
-#jtot = (SS.star.TotAngMom / SS.star.TotAngMom[0] - 1) * 1e11
-
-plt.subplot(3, 2, 6)
-#plt.plot(SS.star.Time, etot, color=vpl.colors.orange, label=(r"Energy $\times 10^9$"))
-#plt.plot(
-#    SS.star.Time, jtot, color=vpl.colors.purple, label=(r"Ang. Mom. $\times 10^{11}$")
-#)
-plt.xlabel("Time (yr)", fontsize=16)
-plt.ylabel(r"$(\Delta E / E - 1)/10^{9}$", fontsize=16)
-plt.ylabel(r"$\Delta$E,  $\Delta$J")
-plt.xlabel("Time (yrs)")
-plt.legend(loc="upper left")
+if cUnitAngle in "deg":
+    cUnitAngle = "$^\circ$"
 
 
-plt.show()
-# Save the figure
-fig.savefig(path / f"SS_NBody.{sys.argv[1]}")
+for iBody in range(len(bodies)):
+    cBodyName = acBodiesNames[iBody]
+    fig, ([ax1, ax2], [ax3, ax4], [ax5, ax6]) = plt.subplots(3, 2, figsize=[14, 16])
+    try: 
+        ax1.plot(bodies[iBody].Time, bodies[iBody].SemiMajorAxis, "k")
+        ax1.set_rasterization_zorder(0)
+        ax1.set_ylabel(f"Semi-Major Axis [{cUnitLength}]")
+        ax1.yaxis.set_major_formatter(FormatStrFormatter("%.6f"))
+
+        ax2.plot(bodies[iBody].Time, bodies[iBody].Eccentricity, "k")
+        ax2.set_ylabel("Eccentricity")
+
+        plt.subplot(3, 2, 3)
+        plt.plot(bodies[iBody].Time, bodies[iBody].Inc, "k")
+        plt.ylabel(f"Inclination [{cUnitAngle}]")
+
+        plt.subplot(3, 2, 4)
+        plt.plot(bodies[iBody].Time, bodies[iBody].LongP, "k", marker=".", linewidth=0)
+        plt.ylim(0, 360)
+        plt.ylabel(f"Longitude of Pericenter [{cUnitAngle}]")
+
+        plt.subplot(3, 2, 5)
+        plt.plot(bodies[iBody].Time, bodies[iBody].LongA, color="k", marker=".", linewidth=0)
+        plt.ylim(0, 360)
+        plt.ylabel(f"Longitude of Ascending Node [{cUnitAngle}]")
+        plt.xlabel(f"Time [{cUnitTime}]")
+
+        try:
+            etot = (bodies[iBody].TotOrbEnergy / bodies[iBody].TotOrbEnergy[0] - 1) * 1e9
+            jtot = (bodies[iBody].TotAngMom / bodies[iBody].TotAngMom[0] - 1) * 1e11
+
+            plt.subplot(3, 2, 6)
+            plt.plot(bodies[iBody].Time, etot, color=vpl.colors.orange, label=(r"Energy $\times 10^9$"))
+            plt.plot(
+                bodies[iBody].Time, jtot, color=vpl.colors.purple, label=(r"Ang. Mom. $\times 10^{11}$")
+            )
+            plt.xlabel(f"Time [{cUnitTime}]", fontsize=16)
+            plt.ylabel(r"$(\Delta E / E - 1)/10^{9}$", fontsize=16)
+            plt.ylabel(r"$\Delta$E,  $\Delta$J")
+            plt.xlabel(f"Time [{cUnitTime}]")
+            plt.legend(loc="upper left")
+        except AttributeError:
+            print(f"WARNING: Conservation values undetected for body {cBodyName}.")
+
+        # Save the figure
+        fig.savefig(path / f"{cBodyName}_OrbElems.{sys.argv[1]}")
+        print(f"Produced figure for body {cBodyName}")
+    except AttributeError:
+        print(f"WARNING: Cannot produce figure for body {cBodyName}.")
+        print(f"Printing out available output options for {cBodyName}:")
+        print(bodies[iBody].__dict__.keys())
